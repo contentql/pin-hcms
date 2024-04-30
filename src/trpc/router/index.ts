afb2ff4c4158d769a14c0c64c8173e9a3e393d85
ABDUL RAHAMAN <36rahaman@gmail.com>
2024-04-30 16:15:19 +0530

    chore: added page router

--- src/trpc/router/index.ts
+++ src/trpc/router/index.ts
@@ -1,8 +1,10 @@
 import { publicProcedure, router } from '@/trpc'
+import { getLayouts } from '@/trpc/router/page-router'
 import { todoRouter } from '@/trpc/router/todo'
 
 export const appRouter = router({
   todo: todoRouter,
+  page:getLayouts,
   test: publicProcedure.query(async () => {
     return {
       success: 'working',
