89b10429f9fd2d4c565693b554c3502679f7b321
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-05-01 23:22:06 +0530

    new: upgraded to v3-beta-21

--- payload.config.ts
+++ payload.config.ts
@@ -5,7 +5,7 @@ import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload/config'
-// import sharp from 'sharp'
+import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 
 import { Blogs } from '@/collections/Blogs'
@@ -30,16 +30,7 @@ export default buildConfig({
   db: mongooseAdapter({
     url: env.DATABASE_URI,
   }),
-
-  // Sharp is now an optional dependency -
-  // if you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-
-  // This is temporary - we may make an adapter pattern
-  // for this before reaching 3.0 stable
-
-  // sharp,
-
+  sharp,
   plugins: [
     s3Storage({
       collections: {
@@ -53,7 +44,6 @@ export default buildConfig({
           secretAccessKey: env.S3_SECRET_ACCESS_KEY,
         },
         region: env.S3_REGION,
-        // ... Other S3 configuration
       },
     }),
   ],
