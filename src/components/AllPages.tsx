3cf95ff741524fa04b3dc37a77aa0b296e0bbf20
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-05-05 23:03:59 +0530

    chore: rendering live preview for blogs

--- src/components/AllPages.tsx
+++ src/components/AllPages.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { env } from '@env'
 import { Blog } from '@payload-types'
+import { useLivePreview } from '@payloadcms/live-preview-react'
 
 import { trpc } from '@/trpc/client'
 
@@ -9,12 +11,22 @@ const AllPages = ({ slug, data }: { slug: string; data: Blog }) => {
     { slug },
     { initialData: data },
   )
+
+  // Fetch blog data for live preview
+  const { data: livePreviewData } = useLivePreview<Blog>({
+    initialData: data, // Use layout as initial data
+    serverURL: env.NEXT_PUBLIC_PUBLIC_URL,
+    depth: 2,
+  })
+
+  // Determine which data to use based on whether live preview data is available
+  const dataToUse = livePreviewData || blog
+
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: blog?.description_html!,
-      }}
-    ></div>
+        __html: dataToUse?.description_html!,
+      }}></div>
   )
 }
 
