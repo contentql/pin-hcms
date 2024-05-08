743cbc6811c62eead545ea394496150f11d9bdd1
jagadeesh <jagadeeshm778@gmail.com>
2024-05-08 18:39:43 +0530

    chore: removed unwanted blocks

--- src/blocks/Page1/index.tsx
+++
@@ -1,5 +0,0 @@
-const Page1 = () => {
-  return <div>This is Home page</div>
-}
-
-export default Page1
--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -6,33 +6,21 @@ import { Container_Scroll } from './Container_Scroll'
 import { Container_Scroll_Block } from './Container_Scroll/block'
 import { HeroParallax } from './HeroParallax'
 import { HeroParallax_Block } from './HeroParallax/block'
-import Page1 from './Page1'
-import { Page1_Block } from './Page1/block'
-// import { StickyScrollReveal } from './StickyScrollBar'
-// import { StickyScrollReveal_Block } from './StickyScrollReveal/block'
-import Test1_1 from './Test1_1'
-import { Test1_1_Block } from './Test1_1/block'
 import { Testimonials } from './Testimonials'
 import { Testimonials_Block } from './Testimonials/block'
 
 export const blocksJSX = {
-  Test1_1: Test1_1,
-  Page1: Page1,
   Testimonials: Testimonials,
   Cards: Cards,
   Container_Scroll: Container_Scroll,
   HeroParallax: HeroParallax,
-  // StickyScrollReveal: StickyScrollReveal,
 }
 
 export type SlugType = keyof typeof blocksJSX
 
 export const blocks = {
-  Test1_1: Test1_1_Block,
-  Page1_Block: Page1_Block,
   Testimonials_Block: Testimonials_Block,
   Cards_Block: Cards_Block,
   Container_Scroll_Block: Container_Scroll_Block,
   HeroParallax_Block: HeroParallax_Block,
-  // StickyScrollReveal_Block: StickyScrollReveal_Block,
 }
