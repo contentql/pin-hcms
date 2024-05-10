f646cf6b814ac8d21be59f36894e15d2a3059251
Git X-Modules <support@tmatesoft.com>
2024-05-10 18:30:04 +0000

    Remove 2 entries
    * module
    * src/test

--- module/HeroParallax/index.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { HeroParallaxTypes } from '@payload-types'
-
-import { HeroParallaxUI } from './hero-parallax'
-
-export function HeroParallax(Hero: HeroParallaxTypes) {
-  return <HeroParallaxUI products={Hero} />
-}
