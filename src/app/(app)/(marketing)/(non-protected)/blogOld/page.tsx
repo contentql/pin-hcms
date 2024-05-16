ec96b278b4232e0bbb5970b639402d6a6a07f320
jagadeesh <jagadeeshm778@gmail.com>
2024-05-16 17:39:31 +0530

    chore: old blog commented

--- src/app/(app)/(marketing)/(non-protected)/blogOld/page.tsx
+++ src/app/(app)/(marketing)/(non-protected)/blogOld/page.tsx
@@ -1,10 +1,10 @@
-import BlogList from '@/app/(app)/(testing)/todo/_components/BlogList'
 import { serverClient } from '@/trpc/serverClient'
 
 const page = async () => {
   const blogsData = await serverClient.blog.getAllBlogs()
 
-  return <BlogList blogs={blogsData} />
+  return ''
+  // <BlogList blogs={blogsData} />
 }
 
 export default page
