e2c16a39d94b8d060f44437c4ba54cd1145396b9
charanm927 <charanm927@gmail.com>
2024-05-20 22:28:54 +0530

    chore: about component dynamic

--- src/payload/blocks/About/index.tsx
+++ src/payload/blocks/About/index.tsx
@@ -1,17 +1,16 @@
-const About = () => {
+import { AboutType, Media } from '~/payload-types'
+
+const About = (data: AboutType) => {
   return (
     <div>
       <section className='bg-white dark:bg-gray-900 pb-10'>
         <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
           <div className='mr-auto place-self-center lg:col-span-7'>
-            <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
-              Welcome To Harare City
+            <h1 className='max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-3xl lg:text-5xl dark:text-white'>
+              {data?.title}
             </h1>
-            <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
-              Welcome to [Company Name], your gateway to unforgettable travel
-              experiences. We are a leading travel and tour company dedicated to
-              creating exceptional journeys for adventurers, explorers, and
-              wanderers like you.
+            <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 text-lg dark:text-gray-400'>
+              {data?.description}
             </p>
             <a
               href='#'
@@ -31,13 +30,13 @@ const About = () => {
             <a
               href='#'
               className='inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'>
-              Speak to Sales
+              {data?.button}
             </a>
           </div>
           <div className='hidden lg:mt-0 lg:col-span-5 lg:flex rounded-lg'>
             <img
-              src='https://lp-cms-production.imgix.net/2019-06/554369495_full.jpg'
-              alt='mockup'
+              src={(data?.image as Media)?.url || ''}
+              alt={(data?.image as Media)?.alt || ''}
               className='rounded-lg'
             />
           </div>
