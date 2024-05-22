16ce9c39c447735f88fa97f1d44c99140073a5df
charanm927 <charanm927@gmail.com>
2024-05-22 12:00:16 +0530

    chore: hero dynamic

--- src/payload/blocks/Hero_2/index.tsx
+++ src/payload/blocks/Hero_2/index.tsx
@@ -11,9 +11,11 @@ import {
 import { useEffect } from 'react'
 import { FiArrowRight } from 'react-icons/fi'
 
+import { Hero_2Type } from '~/payload-types'
+
 const COLORS_TOP = ['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C']
 
-export const Hero_2 = () => {
+export const Hero_2 = (data: Hero_2Type) => {
   const color = useMotionValue(COLORS_TOP[0])
 
   useEffect(() => {
@@ -37,14 +39,13 @@ export const Hero_2 = () => {
       className='relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200'>
       <div className='relative z-10 flex flex-col items-center'>
         <span className='mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm'>
-          Beta Now Live!
+          {data?.caption}
         </span>
         <h1 className='max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight'>
-          Decrease your SaaS churn by over 90%
+          {data?.title}
         </h1>
         <p className='my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
-          distinctio eum impedit nihil ipsum modi.
+          {data?.sub_title}
         </p>
         <motion.button
           style={{
@@ -58,7 +59,7 @@ export const Hero_2 = () => {
             scale: 0.985,
           }}
           className='group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50'>
-          Start free trial
+          {data?.button_text}
           <FiArrowRight className='transition-transform group-hover:-rotate-45 group-active:-rotate-12' />
         </motion.button>
       </div>
