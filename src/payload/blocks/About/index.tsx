5512b18238b317638532adc4abea824b811c321c
charanm927 <charanm927@gmail.com>
2024-05-22 10:41:27 +0530

    chore: modify text color

--- src/payload/blocks/About/index.tsx
+++ src/payload/blocks/About/index.tsx
@@ -14,7 +14,7 @@ const About = (data: AboutType) => {
             </p>
             <a
               href='#'
-              className='inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'>
+              className='inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-gray-900 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'>
               Get started
               <svg
                 className='w-5 h-5 ml-2 -mr-1'
