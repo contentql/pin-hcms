0a8f4f78a6fe4d572b54e71f346f1138a4036fb5
ManikantaPotnuru9176 <manikantapotnur9176@gmail.com>
2024-05-23 23:57:34 +0530

    chore: fetching blogs in client with trpc to get instant load

--- src/app/(app)/(marketing)/(non-protected)/blog/page.tsx
+++ src/app/(app)/(marketing)/(non-protected)/blog/page.tsx
@@ -3,6 +3,7 @@ import { serverClient } from '@/trpc/serverClient'
 
 const page = async () => {
   const blogsData = await serverClient.blog.getAllBlogs()
+
   return <AllBlogs blogsData={blogsData} />
 }
 export default page
