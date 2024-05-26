b5352782f527bd5d2759535115b14cce41e68a54
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-05-27 01:24:03 +0530

    chore: unfinished

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    reactCompiler: false,
+  },
   output: 'standalone',
   reactStrictMode: true,
   compiler: {
