e9a5dab4e8bb5d7a4a04fba81c5f3c81e6228721
jagadeesh <jagadeeshm778@gmail.com>
2024-05-29 17:20:00 +0530

    chore: mobile view updated for header

--- src/payload/blocks/Header/index.tsx
+++ src/payload/blocks/Header/index.tsx
@@ -164,34 +164,17 @@ function Navbar({
                     <ul
                       className='py-2 text-md text-gray-700 dark:text-gray-200'
                       aria-labelledby='doubleDropdownButton'>
-                      <li>
+                        {subMenu?.subMenuItems?.map((nestedMenu,index)=>(
+<li key={index}>
                         <a
-                          href='#'
+                          href={(nestedMenu?.page?.value as Page)?.path || '#'}
                           className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>
-                          Overview
+                          {(nestedMenu?.page?.value as Page)?.slug || '#'}
                         </a>
                       </li>
-                      <li>
-                        <a
-                          href='#'
-                          className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>
-                          My downloads
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href='#'
-                          className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>
-                          Billing
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href='#'
-                          className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>
-                          Rewards
-                        </a>
-                      </li>
+                        ))}
+                      
+                      
                     </ul>
                   </div>
                 </li>):(
