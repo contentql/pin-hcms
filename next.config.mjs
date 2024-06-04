361e22e99f56b416e30f493ba395f67889e6b471
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-06-04 18:07:01 +0530

    chore: updated but still not in working production condition

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
   },
+    experimental: {
+    reactCompiler: false
+  },
   async redirects() {
     return [
       {
