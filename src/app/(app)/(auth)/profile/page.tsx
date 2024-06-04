89309d59640b918ed24165932516a5ccff3e9e02
ManikantaPotnuru9176 <manikantapotnur9176@gmail.com>
2024-06-04 15:29:48 +0530

    chore: updating profile page and its components

--- src/app/(app)/(auth)/profile/page.tsx
+++ src/app/(app)/(auth)/profile/page.tsx
@@ -1,39 +1,31 @@
-import { UserRound } from 'lucide-react'
-import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
-import Container from '@/components/Container'
-import ProfileForm from '@/components/ProfileForm'
 import { getCurrentUser } from '@/lib/payload'
+
+import ProfileForm from '~/src/components/ProfileForm'
 
 const ProfilePage = async () => {
   const user = await getCurrentUser()
+
   if (!user) return redirect('/sign-in')
+
   return (
-    <Container>
-      <div className='pb-10 pt-[100px]'>
-        <div className='grid grid-cols-1 gap-x-3 gap-y-2 sm:grid-cols-4'>
-          <div>
-            <nav>
-              <ul className='flex flex-col gap-y-2 dark:text-zinc-500'>
-                <li>
-                  <Link
-                    href='/profile'
-                    className='flex items-center gap-x-2 transition-all duration-300 ease-in-out hover:text-zinc-900 dark:hover:text-white'
-                  >
-                    <UserRound className='h-5 w-5' />
-                    Profile
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className='col-span-3'>
-            <ProfileForm user={user} />
-          </div>
+    <div className='flex w-full flex-col gap-5 bg-white px-3 text-[#161931] md:flex-row md:px-16 lg:px-28'>
+      <aside className='hidden py-4 md:block md:w-1/3 lg:w-1/4'>
+        <div className='sticky top-12 flex flex-col gap-2 border-r border-indigo-100 p-4 text-sm'>
+          <h2 className='mb-4 pl-3 text-2xl font-semibold'>Settings</h2>
+
+          <a
+            href='#'
+            className='flex items-center px-3 py-2.5 font-semibold  hover:rounded-full hover:border hover:text-indigo-900'>
+            Account Settings
+          </a>
         </div>
-      </div>
-    </Container>
+      </aside>
+      <main className='min-h-screen w-full py-1'>
+        <ProfileForm user={user} />
+      </main>
+    </div>
   )
 }
 
