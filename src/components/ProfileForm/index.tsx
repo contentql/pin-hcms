53329cac67a20288c628f7ea48ce90cfad376f6b
charanm927 <charanm927@gmail.com>
2024-06-18 16:15:51 +0530

    chore: profile theme update

--- src/components/ProfileForm/index.tsx
+++ src/components/ProfileForm/index.tsx
@@ -29,7 +29,9 @@ const ProfileForm = ({ user }: { user: User }) => {
   return (
     <div className='p-2 md:p-4'>
       <div className='mt-8 w-full px-6 pb-8 sm:rounded-lg'>
-        <h2 className='pl-6 text-2xl font-bold sm:text-xl'>Public Profile</h2>
+        <h2 className='pl-6 text-2xl font-bold text-white sm:text-xl'>
+          Public Profile
+        </h2>
 
         <div className='mx-auto mt-8 grid'>
           <div className='flex flex-col items-center space-y-5 sm:flex-row sm:space-y-0'>
@@ -43,7 +45,7 @@ const ProfileForm = ({ user }: { user: User }) => {
             <div className='flex flex-col space-y-5 sm:ml-8'>
               <button
                 type='button'
-                className='rounded-lg border border-indigo-200 bg-[#202142] px-7 py-3.5 text-base font-medium text-indigo-100 hover:bg-indigo-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-indigo-200 '>
+                className='rounded-lg border border-indigo-200 bg-[#26304e] px-7 py-3.5 text-base font-medium text-indigo-100 hover:bg-indigo-600 focus:z-10 focus:outline-none focus:ring-4 focus:ring-indigo-200 '>
                 Change picture
               </button>
               <button
@@ -60,7 +62,7 @@ const ProfileForm = ({ user }: { user: User }) => {
             <div className='mb-4 sm:mb-6'>
               <label
                 htmlFor='name'
-                className='block text-sm font-medium text-gray-700'>
+                className='block text-sm font-medium text-gray-300'>
                 Name
               </label>
               <input
@@ -70,14 +72,14 @@ const ProfileForm = ({ user }: { user: User }) => {
                 placeholder='John'
                 value={formData.name || ''}
                 onChange={handleOnChange}
-                className='mt-1 w-full rounded-md border p-2 transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2'
+                className='mt-1 w-full rounded-md bg-gray-600 p-2 text-white transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-1'
               />
             </div>
 
             <div className='mb-4 sm:mb-6'>
               <label
                 htmlFor='email'
-                className='block text-sm font-medium text-gray-700'>
+                className='block text-sm font-medium text-gray-300'>
                 E-Mail
               </label>
               <input
@@ -87,7 +89,7 @@ const ProfileForm = ({ user }: { user: User }) => {
                 placeholder='john.doe@example.com'
                 value={formData.email}
                 disabled
-                className='mt-1 w-full rounded-md border p-2 transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2'
+                className='mt-1 w-full rounded-md p-2 text-gray-300 transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-1'
               />
             </div>
 
@@ -95,7 +97,7 @@ const ProfileForm = ({ user }: { user: User }) => {
               <div className='w-full'>
                 <label
                   htmlFor='password'
-                  className='block text-sm font-medium text-gray-700'>
+                  className='block text-sm font-medium text-gray-300'>
                   New Password
                 </label>
                 <input
@@ -104,14 +106,14 @@ const ProfileForm = ({ user }: { user: User }) => {
                   name='password'
                   placeholder='● ● ● ● ● ● ● ● ●'
                   onChange={handleOnChange}
-                  className='mt-1 w-full rounded-md border p-2 transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2'
+                  className='mt-1 w-full rounded-md bg-gray-600 p-2 text-white transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-1'
                 />
               </div>
 
               <div className='w-full'>
                 <label
                   htmlFor='confirmPassword'
-                  className='block text-sm font-medium text-gray-700'>
+                  className='block text-sm font-medium text-gray-300'>
                   Confirm Password
                 </label>
                 <input
@@ -120,7 +122,7 @@ const ProfileForm = ({ user }: { user: User }) => {
                   name='confirmPassword'
                   placeholder='● ● ● ● ● ● ● ● ●'
                   onChange={handleOnChange}
-                  className='mt-1 w-full rounded-md border p-2 transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2'
+                  className='mt-1 w-full rounded-md bg-gray-600 p-2 text-white transition-colors duration-300 focus:border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-300 focus:ring-offset-1'
                 />
               </div>
             </div>
@@ -128,7 +130,7 @@ const ProfileForm = ({ user }: { user: User }) => {
             <div className='flex justify-end'>
               <button
                 type='submit'
-                className='w-full rounded-lg  bg-indigo-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 sm:w-auto'>
+                className='w-full rounded-lg  bg-indigo-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 sm:w-auto'>
                 {isPending ? 'Updating...' : 'Update Profile'}
               </button>
             </div>
