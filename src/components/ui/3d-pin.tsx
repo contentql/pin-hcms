f10387a3820ad9c398e71b701ff3067340b81754
jagadeesh <jagadeeshm778@gmail.com>
2024-06-21 16:54:42 +0530

    chore: padding reduced for tags

--- src/components/ui/3d-pin.tsx
+++ src/components/ui/3d-pin.tsx
@@ -48,7 +48,7 @@ export const PinContainer = ({
             perspective: '1000px',
             transform: 'rotateX(70deg) translateZ(0deg)',
           }}
-          className='absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2'>
+          className='absolute left-1/2 top-1/2 ml-[0.09375rem] -translate-x-1/2 -translate-y-1/2'>
           <div
             style={{
               transform: transform,
