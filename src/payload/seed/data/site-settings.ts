9ab2fce4193f707c6556792b54624bbfa54e91be
jagadeesh <jagadeeshm778@gmail.com>
2024-06-21 21:26:11 +0530

    chore: seed date updated for author

--- src/payload/seed/data/site-settings.ts
+++ src/payload/seed/data/site-settings.ts
@@ -23,6 +23,13 @@ export const siteSettings: Omit<SiteSetting, 'id' | 'createdAt' | 'updatedAt'> =
           },
           subMenuItems: [],
         },
+        {
+          page: {
+            value: '',
+            relationTo: 'pages',
+          },
+          subMenuItems: [],
+        },
       ],
     },
     footer: {
