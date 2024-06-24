26f5aec2cc96031ff2a3f2a84125261c779e48cd
charanm927 <charanm927@gmail.com>
2024-06-24 14:40:51 +0530

    chore: delete account

--- src/components/ProfileForm/DeleteAccountSection.tsx
+++ src/components/ProfileForm/DeleteAccountSection.tsx
@@ -124,7 +124,7 @@ export default function DeleteAccountSection() {
                 <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
                   <form action={deleteUserAction}>
                     <button
-                      type='button'
+                      type='submit'
                       disabled={!isAllowedToDelete}
                       className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 disabled:cursor-not-allowed disabled:bg-opacity-50 sm:ml-3 sm:w-auto'>
                       {isPending ? 'Deleting...' : 'Delete Account'}
